9a88a8d2b2bf32f693abdee4de2d51741250e322
jannespph <jannespakpahan645@gmail.com>
2026-02-08 15:02:37 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+?>
+<!doctype html>
+<html>
+<head><meta charset="utf-8"><title>POS</title></head>
+<body>
+    <h1>POS - Master Data</h1>
+    <ul>
+        <li><a href="kategori.php">Master Kategori</a></li>
+        <li><a href="satuan.php">Master Satuan</a></li>
+        <li><a href="produk.php">Master Produk</a></li>
+    </ul>
+</body>
+</html>
